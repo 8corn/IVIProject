348a091b9d6ca2db2c67ba01eb57768e7ef0f631
corn <wjdrnjs1129@naver.com>
2026-05-28 16:16:54 +0900

    navHost 리팩토링

---
+++ app/src/main/java/com/corn/hyundaiproject/presentation/HyundaiNavHost.kt
@@ -0,0 +1,58 @@
+package com.corn.hyundaiproject.presentation
+
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.corn.hyundaiproject.presentation.screen.HomeScreen
+import com.corn.hyundaiproject.presentation.screen.MainScreen
+import com.corn.hyundaiproject.presentation.screen.SettingScreen
+
+@Composable
+fun HyundaiNavHost(
+    modifier: Modifier = Modifier,
+    navController: NavHostController,
+    startDestination: String = "launcher",
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        modifier = modifier,
+        enterTransition = { fadeIn() },
+        exitTransition = { fadeOut() },
+        popEnterTransition = { fadeIn() },
+        popExitTransition = { fadeOut() },
+    ) {
+        composable ("launcher") {
+            HomeScreen (
+                onAppClick = { route ->
+                    when(route) {
+                        "map_screen" -> navController.navigate("main")
+                        "setting_screen" -> navController.navigate("settings")
+                        else -> {
+                            // TODO: 다른 화면 대기
+                        }
+                    }
+                }
+            )
+        }
+        composable ("main") {
+            MainScreen(
+                onSettingsClick = {
+                    navController.navigate("settings")
+                }
+            )
+        }
+        composable("settings") {
+            SettingScreen(
+                onBackClick = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
--- app/src/main/java/com/corn/hyundaiproject/presentation/MainActivity.kt
+++ app/src/main/java/com/corn/hyundaiproject/presentation/MainActivity.kt
@@ -3,13 +3,7 @@ package com.corn.hyundaiproject.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import com.corn.hyundaiproject.presentation.screen.HomeScreen
-import com.corn.hyundaiproject.presentation.screen.MainScreen
-import com.corn.hyundaiproject.presentation.screen.SettingScreen
+import androidx.navigation.compose.rememberNavController
 import com.corn.hyundaiproject.presentation.ui.theme.HyundaiProjectTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,28 +13,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            var currentScreen by remember { mutableStateOf("launcher") }
-
             HyundaiProjectTheme {
-                when (currentScreen) {
-                    "launcher" -> HomeScreen(
-                        onAppClick = { route ->
-                            when (route) {
-                                "map_screen" -> currentScreen = "main"
-                                "setting_screen" -> currentScreen = "settings"
-                                else -> {
-                                    // Todo: 라디오, 미디어 등
-                                }
-                            }
-                        }
-                    )
-                    "main" -> MainScreen(
-                        onSettingsClick = { currentScreen = "settings" }
-                    )
-                    "settings" -> SettingScreen(
-                        onBackClick = { currentScreen = "main" }
-                    )
-                }
+                val navController = rememberNavController()
+
+                HyundaiNavHost(navController = navController)
             }
         }
     }
