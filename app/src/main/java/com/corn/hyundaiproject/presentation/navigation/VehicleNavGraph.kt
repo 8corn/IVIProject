fc39a398622b313cd0079d61ca1dd1ef18fbda65
corn <wjdrnjs1129@naver.com>
2026-06-19 21:39:44 +0900

    세팅 스크린 연동 페이지 생성

--- app/src/main/java/com/corn/hyundaiproject/presentation/navigation/VehicleNavGraph.kt
+++ app/src/main/java/com/corn/hyundaiproject/presentation/navigation/VehicleNavGraph.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.corn.hyundaiproject.presentation.screen.DashboardScreen
 import com.corn.hyundaiproject.presentation.screen.SettingScreen
 
 @Composable
@@ -16,7 +17,14 @@ fun VehicleNavGraph(
         startDestination = VehicleRoutes.DASHBOARD
     ) {
         composable (route = VehicleRoutes.DASHBOARD) {
-            // 메인 대시보드 화면 주소 등록
+            DashboardScreen(
+                onNavigateToSettings = {
+                    navController.navigate(VehicleRoutes.SETTINGS)
+                },
+                onNavigateToHvac = {
+                    navController.navigate(VehicleRoutes.HVAC)
+                }
+            )
         }
 
         composable (route = VehicleRoutes.SETTINGS) {
