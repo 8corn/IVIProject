cb979b194c4b5f4538cec9c6d5f01649457f04d3
corn <wjdrnjs1129@naver.com>
2026-06-20 13:58:39 +0900

    세팅 스크린 연동 페이지 생성

--- app/src/main/java/com/corn/hyundaiproject/presentation/screen/DashboardScreen.kt
+++ app/src/main/java/com/corn/hyundaiproject/presentation/screen/DashboardScreen.kt
@@ -5,12 +5,19 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.corn.hyundaiproject.presentation.viewModel.CarViewModel
 
 @Composable
 fun DashboardScreen(
+    carViewModel: CarViewModel = hiltViewModel(),
     onNavigateToSettings: () -> Unit,
     onNavigateToHvac: () -> Unit
 ) {
+    val details by carViewModel.vehicleDetails.collectAsState()
+
     IconButton(
         onClick = onNavigateToSettings
     ) {
