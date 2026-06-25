562aa6fbfbfcb569e7606f910be8ffe26985196f
corn <wjdrnjs1129@naver.com>
2026-06-25 22:14:47 +0900

    세팅 스크린 연동 페이지 생성

--- app/src/main/java/com/corn/hyundaiproject/presentation/screen/HvacScreen.kt
+++ app/src/main/java/com/corn/hyundaiproject/presentation/screen/HvacScreen.kt
@@ -1,6 +1,8 @@
 package com.corn.hyundaiproject.presentation.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.corn.hyundaiproject.presentation.viewModel.CarViewModel
 
@@ -9,5 +11,9 @@ fun HvacScreen(
     carViewModel: CarViewModel = hiltViewModel(),
     onBackClick: () -> Unit
 ) {
+    val hvacState by carViewModel.hvacInfo.collectAsState()
 
+    val currentTemp = hvacState?.temperature ?: 22.0f
+    val warningMessage = hvacState?.warningMessage ?: "실내 온도가 쾌적한 상태입니다."
+    val isLocked = hvacState?.isDoorLocked ?: false
 }
