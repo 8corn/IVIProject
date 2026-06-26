336eb0f7b24e5daba7dcd0c69bbc6bf5456f5218
corn <wjdrnjs1129@naver.com>
2026-06-26 21:11:29 +0900

    세팅 스크린 연동 페이지 생성

--- app/src/main/java/com/corn/hyundaiproject/presentation/screen/HvacScreen.kt
+++ app/src/main/java/com/corn/hyundaiproject/presentation/screen/HvacScreen.kt
@@ -1,9 +1,19 @@
 package com.corn.hyundaiproject.presentation.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.corn.hyundaiproject.presentation.ui.theme.HyundaiPureBlack
 import com.corn.hyundaiproject.presentation.viewModel.CarViewModel
 
 @Composable
@@ -16,4 +26,17 @@ fun HvacScreen(
     val currentTemp = hvacState?.temperature ?: 22.0f
     val warningMessage = hvacState?.warningMessage ?: "실내 온도가 쾌적한 상태입니다."
     val isLocked = hvacState?.isDoorLocked ?: false
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(HyundaiPureBlack)
+            .padding(24.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+        ) { }
+    }
 }
