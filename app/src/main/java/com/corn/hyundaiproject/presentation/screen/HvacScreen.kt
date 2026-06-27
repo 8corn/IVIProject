f25b04ebd538fff6bcaf78a5968ff24bc01145eb
corn <wjdrnjs1129@naver.com>
2026-06-27 20:23:31 +0900

    세팅 스크린 연동 페이지 생성

--- app/src/main/java/com/corn/hyundaiproject/presentation/screen/HvacScreen.kt
+++ app/src/main/java/com/corn/hyundaiproject/presentation/screen/HvacScreen.kt
@@ -3,17 +3,26 @@ package com.corn.hyundaiproject.presentation.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.corn.hyundaiproject.presentation.ui.theme.HyundaiPureBlack
+import com.corn.hyundaiproject.presentation.ui.theme.MetallicSilver
 import com.corn.hyundaiproject.presentation.viewModel.CarViewModel
 
 @Composable
@@ -36,7 +45,20 @@ fun HvacScreen(
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(56.dp)
-        ) { }
+                .height(56.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(
+                onClick = onBackClick
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back",
+                    tint = MetallicSilver
+                )
+            }
+
+            Spacer(modifier = Modifier.width(20.dp))
+        }
     }
 }
