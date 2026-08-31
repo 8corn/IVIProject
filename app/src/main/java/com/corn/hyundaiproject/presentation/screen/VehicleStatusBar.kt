d44f2c26e16a73111d17e525c7f7a230a45e7cf2
corn <wjdrnjs1129@naver.com>
2026-08-31 21:23:50 +0900

    Status Bar 생성

--- app/src/main/java/com/corn/hyundaiproject/presentation/screen/VehicleStatusBar.kt
+++ app/src/main/java/com/corn/hyundaiproject/presentation/screen/VehicleStatusBar.kt
@@ -1,12 +1,17 @@
 package com.corn.hyundaiproject.presentation.screen
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun VehicleStatusBar(
     outsideTemp: String = "20.5",
     userName: String = "Driver 1"
 ) {
-    Row() { }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) { }
 }
