c418eceba9f6831393bf02f48308124079ed1de5
corn <wjdrnjs1129@naver.com>
2026-09-06 21:07:31 +0900

    Status Bar 생성

--- app/src/main/java/com/corn/hyundaiproject/presentation/screen/VehicleStatusBar.kt
+++ app/src/main/java/com/corn/hyundaiproject/presentation/screen/VehicleStatusBar.kt
@@ -3,9 +3,12 @@ package com.corn.hyundaiproject.presentation.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -42,6 +45,10 @@ fun VehicleStatusBar(
                 color = Color.White,
                 fontSize = 14.sp
             )
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Icon()
         }
     }
 }
